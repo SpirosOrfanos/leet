9d75e19706b07b1daba80b528ad94d6513d211c5
ORFANOS Spiros <sorfanos@intrasoft-intl.com>
2021-11-22 17:16:04 +0200

    Fib, Rain, Trie

--- src/easytasks/Fibonacci.java
+++ src/easytasks/Fibonacci.java
@@ -1,9 +1,16 @@
 package easytasks;
 
+/*
+https://leetcode.com/problems/fibonacci-number/
+https://leetcode.com/problems/n-th-tribonacci-number/
+ */
 public class Fibonacci {
     public static void main(String... args) {
         Fibonacci app = new Fibonacci();
-        System.out.println(app.fib(2));
+       // System.out.println(app.fib(2));
+       // System.out.println(app.fib(3));
+        System.out.println(app.fib(4));
+        System.out.println(app.fibSeries(25));
     }
 
     public int fib(int n) {
@@ -16,6 +23,23 @@ public class Fibonacci {
             count++;
         }
 
+         //   System.out.println(sum);
+        return arr[n];
+    }
+
+    public int fibSeries(int n) {
+        if (n<1) return 0;
+        if (n<3) return 1;
+        int[] arr = new int[n+1];
+        arr[1] = 1;
+        arr[2] = 1;
+        int count = 3;
+        while (count<n+1) {
+            arr[count] = arr[count-1] + arr[count-2]+arr[count-3];
+            count++;
+        }
+
+        //   System.out.println(sum);
         return arr[n];
     }
 }
