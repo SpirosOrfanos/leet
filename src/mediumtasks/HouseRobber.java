1dafb9b9e6917ddd703df6a5a96f6a9972f76e96
ORFANOS Spiros <sorfanos@intrasoft-intl.com>
2021-12-02 10:22:20 +0200

    ne

--- src/mediumtasks/HouseRobber.java
+++ src/mediumtasks/HouseRobber.java
@@ -1,9 +1,28 @@
 package mediumtasks;
 
+//https://leetcode.com/problems/house-robber/
 public class HouseRobber {
-    public int rob(int[] nums) {
+    public static void main(String... args) {
+        HouseRobber app = new HouseRobber();
+      //  System.out.println(app.rob(new int[] {1,2,3,1,1}));
+     //   System.out.println(app.rob(new int[] {1,2,3,1}));
+        System.out.println(app.rob(new int[] {2,1,1,2}));
 
-
-        return  -1;
     }
+    public int rob(int[] nums) {
+        if (nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+        if (nums.length == 2) return Math.max(nums[0], nums[1]);
+        if (nums.length == 3) return Math.max(nums[0]+nums[2], nums[1]);
+        nums[1] = Math.max(nums[1], nums[0]);
+        nums[2] = Math.max(nums[2]+nums[0], nums[1]);
+        int max = Math.max(nums[2], nums[1]);
+        for( int i = 3; i< nums.length; i++) {
+            max = Math.max(nums[i]+nums[i-2], nums[i-1]);
+            nums[i] = max;
+        }
+        return  max;
+    }
+
+
 }
---
+++ src/mediumtasks/LongestSubstringWitjoutRepeatingCharacters.java
@@ -0,0 +1,20 @@
+package mediumtasks;
+
+public class LongestSubstringWitjoutRepeatingCharacters {
+
+    public static void main(String... args) {
+        LongestSubstringWitjoutRepeatingCharacters app = new LongestSubstringWitjoutRepeatingCharacters();
+    System.out.println(app.lengthOfLongestSubstring("abcabcbb"));
+    }
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() ==0) return 0;
+        int[] observer = new int[128];
+        int max = 0;
+        for (int i = 0,j = 0; j<s.length(); j++) {
+            i = Math.max(observer[s.charAt(j)], i);
+            max = Math.max(max, j-i+1);
+            observer[s.charAt(j)] = j+1;
+        }
+        return max;
+    }
+}
